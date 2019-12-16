5dfbb86ac7b4eee77be1312582875d2da22bcfef
Daniel Wegmann <daniel.wegmann@gmail.com>
2019-12-16 22:46:29 +0100

    added token auth for user API

--- app/api/users.py
+++ app/api/users.py
@@ -5,12 +5,16 @@ from flask import request
 from flask import url_for
 from app import db
 from app.api.errors import bad_request
+from flask import g, abort
+from app.api.auth import token_auth
 
 @bp.route('/users/<int:id>', methods=['GET'])
+@token_auth.login_required
 def get_user(id):
     return jsonify(User.query.get_or_404(id).to_dict())
 
 @bp.route('/users', methods=['GET'])
+@token_auth.login_required
 def get_users():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
@@ -18,6 +22,7 @@ def get_users():
     return jsonify(data)
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
+@token_auth.login_required
 def get_followers(id):
     user = User.query.get_or_404(id)
     page = request.args.get('page', 1, type=int)
@@ -27,6 +32,7 @@ def get_followers(id):
     return jsonify(data)
 
 @bp.route('/users/<int:id>/followed', methods=['GET'])
+@token_auth.login_required
 def get_followed(id):
     user = User.query.get_or_404(id)
     page = request.args.get('page', 1, type=int)
@@ -54,7 +60,10 @@ def create_user():
     return response
 
 @bp.route('/users/<int:id>', methods=['PUT'])
+@token_auth.login_required
 def update_user(id):
+    if g.current_user.id != id:
+        abort(403)
     user = User.query.get_or_404(id)
     data = request.get_json() or {}
     if 'username' in data and data['username'] != user.username and \
